a1f2e82cd7ff259bafd01e35264c64d864e3a9c4
Yapcc00 <yeison.parra@parqueexplora.org>
2023-06-02 14:27:33 -0500

    update

--- app/components/modules/CardProgramate.jsx
+++ app/components/modules/CardProgramate.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import ExampleCard from "./ExamplesCards";
 
 export const CardProgramate = () => {
-  const exampleCodeFontsInLine = ` <div className="cardp is-active">
+  const exampleCodeFontsInLine = `      <div className="cardp is-active">
   <div className="cardp-header ">
     <Image
       src="/assets/icons-learn/img.jpeg"
@@ -17,19 +17,27 @@ export const CardProgramate = () => {
       <p aria-hidden="true" className="cardp-header-audience-modality"> Presencial </p>
     </div>
   </div>
-  
-  <div className="cardp-bodyDate">
-    <div className="cardp-bodyDate-itemDate">
-      <h3 className="cardp-bodyDate-itemDate-day"> 2 </h3>
-      <h3 className="cardp-bodyDate-itemDate-month"> JUNIO </h3>
+
+  <div className="cardp-body">
+    <div className="cardp-body-date">
+      <h3 className="cardp-body-date-day"> 2 </h3>
+      <h3 className="cardp-body-date-month"> JUNIO </h3>
+    </div>
+    <div>
+      <div className="cardp-body-date-itemNow">
+        <span className="icon-explora icon-location-c"></span>
+        <span className=""> Ocurre ahora </span>
+      </div>
     </div>
   </div>
+
   <span className="separatorCard"></span>
   <div className="cardp-infoCard">
-    <p aria-label={"Hora del evento: 3:00 pm"}className="cardp-infoCard-hour"> 3:00 p.m. </p>
-    <h4 className="cardp-infoCard-title"> EJM: EJEMPLO CARD </h4>
-    <button className="cardp-infoCard-btn"> VER MÁS </button>
+    <p aria-label={"Hora del evento: 3:00 pm"} className="cardP-infoCard-hour" > 3:00 p.m. </p>
+    <h4 className="cardp-infoCard-title"> ejm: card ejemplo</h4>
+    <button className="cardp-infoCard-btn"> ver más  </button>
   </div>
+  
 </div> `;
 
   return (
@@ -51,44 +59,30 @@ export const CardProgramate = () => {
               width="298"
               height="200"
             />
-            <div aria-hidden="true" className="cardp-header-location">
-              {" "}
-              Planetario{" "}
-            </div>
+            <div aria-hidden="true" className="cardp-header-location"> Planetario </div>
             <div className="cardp-header-audience">
-              <p aria-hidden="true" className="cardp-header-audience-tag">
-                {" "}
-                Jóvenes y adultos{" "}
-              </p>
-              <p aria-hidden="true" className="cardp-header-audience-modality">
-                Presencial
-              </p>
+              <p aria-hidden="true" className="cardp-header-audience-tag"> Jóvenes y adultos </p>
+              <p aria-hidden="true" className="cardp-header-audience-modality"> Presencial </p>
             </div>
           </div>
 
           <div className="cardp-body">
             <div className="cardp-body-date">
-              <h3 className="cardp-body-date-day">2</h3>
-              <h3 className="cardp-body-date-month">JUNIO</h3>
+              <h3 className="cardp-body-date-day"> 2 </h3>
+              <h3 className="cardp-body-date-month"> JUNIO </h3>
             </div>
             <div>
               <div className="cardp-body-date-itemNow">
                 <span className="icon-explora icon-location-c"></span>
-                <span className="">Ocurre ahora</span>
+                <span className=""> Ocurre ahora </span>
               </div>
             </div>
           </div>
           <span className="separatorCard"></span>
           <div className="cardp-infoCard">
-            <p
-              aria-label={`Hora del evento: 3:00 pm`}
-              className="cardP-infoCard-hour"
-            >
-              {" "}
-              3:00 p.m.{" "}
-            </p>
-            <h4 className="cardp-infoCard-title">EJM: EJEMPLO CARD</h4>
-            <button className="cardp-infoCard-btn">VER MÁS</button>
+            <p aria-label={`Hora del evento: 3:00 pm`} className="cardP-infoCard-hour" > 3:00 p.m. </p>
+            <h4 className="cardp-infoCard-title"> ejm: card ejemplo</h4>
+            <button className="cardp-infoCard-btn"> ver más  </button>
           </div>
         </div>
 
--- app/components/modules/CardProjects.jsx
+++ app/components/modules/CardProjects.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 import Image from "next/image";
+import ExampleCard from "./ExamplesCards";
 
 export const CardProjects = () => {
+  const exampleCodeFontsInLine = ` <div className="cardProyects">
+  <div className="cardProyects-img">
+    <Image
+      src="/assets/icons-learn/hd.jpeg"
+      alt="Imagen"
+      width="330"
+      height="240"
+    />
+  </div>
+
+  <div className="cardProyects-body">
+
+    <h3 className="cardProyects-body-title"> EJEMPLO TITULO </h3>
+
+    <p className="cardProyects-body-info">
+      popularizado en los 60s con la creación de las hojas etraset, las
+      cuales contenian pasajes de Lorem Ipsum, y más recientemente con
+      software de autoedición, como por ejemplo Aldus PageMaker, el cual
+      incluye versiones de Lorem Ipsum.popularizado en los 60s con la
+    </p>
+
+    <button className="cardProyects-body-btn"> ver más </button>
+
+  </div>
+</div>`;
+
   return (
     <div className="itemCards">
       <div className="titles">
@@ -28,11 +55,16 @@ export const CardProjects = () => {
             cuales contenian pasajes de Lorem Ipsum, y más recientemente con
             software de autoedición, como por ejemplo Aldus PageMaker, el cual
             incluye versiones de Lorem Ipsum.popularizado en los 60s con la
-       
           </p>
           <button className="cardProyects-body-btn">ver más</button>
         </div>
       </div>
+      <br />
+      <br />
+      <ExampleCard
+        exampleCode={exampleCodeFontsInLine}
+        language={"typescript"}
+      ></ExampleCard>
     </div>
   );
 };
