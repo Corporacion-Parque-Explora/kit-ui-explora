13e23edfbc77c4094803c1259812324d3750ba8b
Yapcc00 <yeison.parra@parqueexplora.org>
2023-06-06 10:10:23 -0500

    Cards info and separatorTitle

--- app/components/modules/CardInfo.jsx
+++ app/components/modules/CardInfo.jsx
@@ -1,37 +1,133 @@
-import Image from "next/image";
+import ExampleCard from "./ExamplesCards";
 
+import Image from "next/image";
+const exampleCodeFontsInLine = `   <div className="cardInfo-header">
+<div className="cardInfo-body">
+  <Image
+    aria-hidden="true"
+    src="/assets/icons/icon-time2.webp"
+    alt="icon"
+    width={"140"}
+    height={"150"}
+  />
+
+  <h3 className="cardInfo-body-title">HORARIOS Y TARIFAS</h3>
+
+  <a
+    role="button"
+    aria-label="Ver mas sobre HORARIOS Y TARIFAS"
+    className="cardInfo-body-button"
+    href=""
+  >
+    VER MÁS
+  </a>
+</div>
+</div> `;
 export default function CardsInfo() {
   return (
-    <div className="informationInterest">
+    <div className="cardInfo">
       <div>
-        <div className="itemCards">
+        <div className="itemCards-info">
           <div className="titles">
             <div className="title1">
               <h3 className="fs-5">Card Info</h3>
               <hr />
             </div>
           </div>
-          <div className="CardsInfo">
-            <div className="cardInfo">
+
+          <div className="cardInfo-header">
+            <div className="cardInfo-body">
               <Image
                 aria-hidden="true"
                 src="/assets/icons/icon-time2.webp"
                 alt="icon"
-                width={"140"}
+                width={"150"}
                 height={"150"}
               />
 
-              <h3 className="infoTitle">HORARIOS Y TARIFAS</h3>
-              
+              <h3 className="cardInfo-body-title">HORARIOS Y TARIFAS</h3>
+
               <a
                 role="button"
                 aria-label="Ver mas sobre HORARIOS Y TARIFAS"
-                className="infoButton"
-                href="">
+                className="cardInfo-body-button"
+                href=""
+              >
+                VER MÁS
+              </a>
+            </div>
+
+            <div className="cardInfo-body">
+              <Image
+                aria-hidden="true"
+                src="/assets/icons/location.webp"
+                alt="icon"
+                width={"120"}
+                height={"160"}
+              />
+
+              <h3 className="cardInfo-body-title">CÓMO LLEGAR</h3>
+
+              <a
+                role="button"
+                aria-label="Ver mas sobre HORARIOS Y TARIFAS"
+                className="cardInfo-body-button"
+                href=""
+              >
+                VER MÁS
+              </a>
+            </div>
+
+            <div className="cardInfo-body">
+              <Image
+                aria-hidden="true"
+                src="/assets/icons/icon-people.webp"
+                alt="icon"
+                width={"200"}
+                height={"150"}
+              />
+
+              <h3 className="cardInfo-body-title">INGRESO GRATUITO</h3>
+
+              <a
+                role="button"
+                aria-label="Ver mas sobre HORARIOS Y TARIFAS"
+                className="cardInfo-body-button"
+                href=""
+              >
+                VER MÁS
+              </a>
+            </div>
+
+            <div className="cardInfo-body">
+              <Image
+                aria-hidden="true"
+                src="/assets/icons/icon-percentage.webp"
+                alt="icon"
+                width={"130"}
+                height={"150"}
+              />
+
+              <h3 className="cardInfo-body-title">DESCUENTOS</h3>
+
+              <a
+                role="button"
+                aria-label="Ver mas sobre HORARIOS Y TARIFAS"
+                className="cardInfo-body-button"
+                href=""
+              >
                 VER MÁS
               </a>
             </div>
           </div>
+          <br />
+          <br />
+          <br />
+
+          <ExampleCard
+            exampleCode={exampleCodeFontsInLine}
+            language={"javascript"}
+          ></ExampleCard>
         </div>
       </div>
     </div>
